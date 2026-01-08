81415d0b36b732b3bd161ab5963dee563eaa802f
JimmyGAR <jimmy.garnier1110@gmail.com>
2026-01-08 10:28:21 +0100

    L as Liskov Subtitution Principle

---
+++ exo-solid-3/Mp3.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'MusicType.php';
+require_once("InvalidExtensionException.php");
+
+class Mp3 extends MusicType
+{
+    public function listen()
+    {
+        $extension = pathinfo($this->filename, PATHINFO_EXTENSION);
+        if ($extension !== 'mp3') {
+            throw new InvalidExtensionException("Fichier Mp3 attendu mais ''$extension'' obtenu");
+        }
+
+        return 'Lecture du fichier Mp3 '. $this->filename;
+    }
+}
